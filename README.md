28a262febdba851bf3b18b6ac07e3588f0dbdfb4
oritzau <oritzau2021@gmail.com>
2023-03-14 23:14:40 -0400

    Now accepts command line arguments

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ How to use:
 run `cargo build --release` to compile into an executable, then 
 use `~/path/to/run file_name` to run the desired file in the current directory. 
 
-On linux, I recommend moving run to ~/usr/bin so it can be used as a global command.
+On linux, I recommend moving run to `~/usr/bin` so it can be used as a global command.
 
-Ex: `run main.py`, `run hello_world.cpp`, `run Calculator.java`
+Ex: `run main.py`, `run hello_world.cpp`, `run Calculator.java -e foo`
 
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use std::process::Command;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        panic!("Pass in exactly one file name");
+    if args.len() <= 2 {
+        panic!("Pass in file name");
     }
     let file_name = args.get(1).unwrap();
     let split_file_name: Vec<&str> = args.get(1).unwrap().split('.').collect();
@@ -21,20 +21,21 @@ fn main() {
         "js" => Command::new("nodejs"),
         _ => panic!("Ending not found"),
     };
+    command.arg(file_name);
+    if args.len() > 2 {
+        command.args(&args[2..]);
+    }
     if is_c_like {
-        command.arg(file_name)
-            .arg("-o")
-            .arg("output")
-            .current_dir(&dir)
-            .status()
-            .expect("Failed to spawn command");
-        _ = Command::new("./output")
-            .current_dir(&dir)
-            .status()
-            .expect("Failed to spawn command");
+        command
+            .args(["-o", "output"])
+            .current_dir(dir);
     } else {
-        command.arg(file_name)
-            .current_dir(&dir)
+        command
+            .current_dir(dir);
+    }
+    command.status().expect("Failed to spawn command");
+    if is_c_like {
+        _ = Command::new("./output")
             .status()
             .expect("Failed to spawn command");
     }
