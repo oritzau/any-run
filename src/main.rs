460d37440e3f41baa867dafd0eb8e9f223654a24
oritzau <oritzau2021@gmail.com>
2023-03-18 18:48:58 -0400

    Added support for Rust files

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::process::Command;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() <= 2 {
+    if args.len() < 2 {
         panic!("Pass in file name");
     }
     let file_name = args.get(1).unwrap();
@@ -13,7 +13,11 @@ fn main() {
     let mut is_c_like = false;
     let mut command = match file_ending {
         "py" => Command::new("python3"),
-        "java" => Command::new("java"), 
+        "java" => Command::new("java"),
+        "rs" => {
+            is_c_like = true;
+            Command::new("rustc")
+        }
         "c" | "cpp" => {
             is_c_like = true;
             Command::new("g++")
