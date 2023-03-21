2f5291c03e3b48c83406123d4b7143bd0df942fc
oritzau <oritzau2021@gmail.com>
2023-03-20 22:11:14 -0400

    Fixed issue causing multiple periods in filename, ie main.foo.bar.c breaking match logic

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,33 @@ use std::process::Command;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        panic!("Pass in file name");
+        panic!("Pass in file name, eg. 'run main.c' or 'run app.js -r -s'");
     }
+    
     let file_name = args.get(1).unwrap();
     let split_file_name: Vec<&str> = args.get(1).unwrap().split('.').collect();
-    let file_ending: &str = split_file_name.get(1).unwrap();
+    let file_ending: &str = split_file_name.last().unwrap();
     let dir = env::current_dir().unwrap();
-    let mut is_c_like = false;
+    let mut requires_second_file = false;
     let mut command = match file_ending {
         "py" => Command::new("python3"),
         "java" => Command::new("java"),
         "rs" => {
-            is_c_like = true;
+            requires_second_file = true;
             Command::new("rustc")
         }
         "c" | "cpp" => {
-            is_c_like = true;
+            requires_second_file = true;
             Command::new("g++")
         }
         "js" => Command::new("nodejs"),
-        _ => panic!("Ending not found"),
+        _ => panic!("File ending not found, see https://github.com/oritzau/any-run/blob/master/README.md for supported file types"),
     };
     command.arg(file_name);
     if args.len() > 2 {
         command.args(&args[2..]);
     }
-    if is_c_like {
+    if requires_second_file {
         command
             .args(["-o", "output"])
             .current_dir(dir);
@@ -38,7 +39,7 @@ fn main() {
             .current_dir(dir);
     }
     command.status().expect("Failed to spawn command");
-    if is_c_like {
+    if requires_second_file {
         _ = Command::new("./output")
             .status()
             .expect("Failed to spawn command");
