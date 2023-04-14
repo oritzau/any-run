c6305fdc9d31b64d12fc3d30833f28899bf4461e
oritzau <oritzau2021@gmail.com>
2023-04-14 10:54:35 -0400

    Optimized Codefile::new(), removed use of clone and changed fields to &str

--- src/lib.rs
+++ src/lib.rs
@@ -1,80 +1,75 @@
 pub mod code_file {
     use std::path::PathBuf;
-    
+
     use std::env;
     use std::process::Command;
 
-    pub struct Codefile {
-        pub name: String,
-        pub ending: String, 
+    pub struct Codefile<'a> {
+        pub name: &'a str,
+        pub ending: &'a str,
         pub dir: PathBuf,
-        pub command: Vec<String>,
+        pub command: Vec<&'a str>,
         pub compiled: bool,
-        pub target_name: String,
-    } 
+        pub target_name: &'a str,
+    }
 
     pub fn get_filename_index(args: &Vec<String>) -> usize {
         let index = 1;
         if args[1].starts_with('-') {
-                return index + 2;
-            }
+            return index + 2;
+        }
         index
     }
 
-    pub fn get_file_ending(file_name: String) -> Option<String> {
+    pub fn get_file_ending(file_name: &str) -> Option<&str> {
         let split_file_name: Vec<&str> = file_name.split(".").collect();
         let ending = split_file_name.last()?;
-        Some(ending.to_string())
+        Some(ending)
     }
-    
-    impl Codefile {
-        pub fn new(args: Vec<String>, file_name_index: usize) -> Option<Codefile> {
+
+    impl<'a> Codefile<'a> {
+        pub fn new(args: &'a Vec<String>, file_name_index: usize) -> Option<Codefile<'a>> {
             if args.len() == 0 {
                 return None;
             }
-            let name = args.get(file_name_index)?.to_owned();
-            let split_file_name: Vec<&str> = args.get(file_name_index)?.split(".").collect();
-            let ending: String = split_file_name.last()?.to_string();
-            let dir = env::current_dir().ok()?;            
-            let (compiled, mut command) = match ending.as_str() {
+            let name = args.get(file_name_index)?;
+            let ending = get_file_ending(&name)?;
+            let dir = env::current_dir().ok()?;
+            let (compiled, mut command) = match ending {
                 "py" => {
-                if env::consts::OS == "windows" {
-                    (false, vec![String::from("python")])
-                } else {
-                    (false, vec![String::from("python3")])
+                    if env::consts::OS == "windows" {
+                        (false, vec![("python")])
+                    } else {
+                        (false, vec![("python3")])
+                    }
                 }
-                },
-                "java" => (false, vec![String::from("java")]),
-                "rs" => (true, vec![String::from("rustc")]),
-                "c" => (true, vec![String::from("gcc")]),
-                "cpp" => (true, vec![String::from("g++")]),
-                "js" => (false, vec![String::from("node")]),
+                "java" => (false, vec!["java"]),
+                "rs" => (true, vec!["rustc"]),
+                "c" => (true, vec!["gcc"]),
+                "cpp" => (true, vec!["g++"]),
+                "js" => (false, vec!["node"]),
                 _ => panic!(
                     "File ending not supported, see 
                     https://github.com/oritzau/any-run/blob/master/README.md 
                     for supported file types"
                 ),
             };
-            let target_name: String = if args[1].as_str() == "-o" {
-                args[2].clone()
-            } else {
-                String::from("output")
-            };
-            
+            let target_name: &str = if args[1] == "-o" { &args[2] } else { "output" };
+
             for arg in &args[1..file_name_index] {
-                command.push(arg.to_owned());
+                command.push(&arg);
             }
 
             if compiled && command.len() == 1 {
-                command.push("-o".to_string());
-                command.push(target_name.clone())
+                command.push("-o");
+                command.push(target_name)
             }
 
             for arg in &args[file_name_index..] {
-                command.push(arg.to_owned())
+                command.push(&arg)
             }
-                    
-            Some(Codefile {
+
+            Some(Self {
                 name,
                 ending,
                 dir,
@@ -83,7 +78,7 @@ pub mod code_file {
                 target_name,
             })
         }
-    
+
         pub fn spawn(self) {
             let _ = Command::new(&self.command[0])
                 .args(&self.command[1..])
@@ -95,8 +90,7 @@ pub mod code_file {
                     .status()
                     .expect("Failed to spawn secondary command");
             }
-            }
         }
     }
-
+}
 
--- tests/test.rs
+++ tests/test.rs
@@ -2,28 +2,31 @@ use run::code_file::*;
 
 #[test]
 fn file_name_works() {
-    let file = Codefile::new(vec!["run".to_string(), "main.py".to_string()], 1);
+    let vec = vec!["run".to_string(), "main.py".to_string()];
+    let file = Codefile::new(&vec, 1);
     assert_eq!(file.unwrap().name, String::from("main.py"));
 }
 
 #[test]
 fn file_ending_works() {
-    let file = Codefile::new(vec!["run".to_string(), "main.foo.bar.c".to_string()], 1);
+    let vec = vec!["run".to_string(), "main.foo.bar.c".to_string()];
+    let file = Codefile::new(&vec, 1);
     assert_eq!(file.unwrap().ending, String::from("c"));
 }
 
 #[test]
 fn command_works_cross_platform() {
-    let file = Codefile::new(vec!["run".to_string(), "main.py".to_string()], 1);
+    let vec = vec!["run".to_string(), "main.py".to_string()];
+    let file = Codefile::new(&vec, 1);
     match std::env::consts::OS {
-        "linux" | "macos" => assert_eq!(file.unwrap().command, vec![
-            String::from("python3"),
-            String::from("main.py")
-        ]),
-        "windows" => assert_eq!(file.unwrap().command, vec![
-            String::from("python"),
-            String::from("main.py")
-        ]),
+        "linux" | "macos" => assert_eq!(
+            file.unwrap().command,
+            vec![String::from("python3"), String::from("main.py")]
+        ),
+        "windows" => assert_eq!(
+            file.unwrap().command,
+            vec![String::from("python"), String::from("main.py")]
+        ),
         _ => panic!("Invalid OS detected"),
     }
 }
@@ -37,23 +40,29 @@ fn file_renamed_with_arg() {
         "main.c".to_string(),
     ];
     assert_eq!(get_filename_index(&vec), 3);
-    assert_eq!(Codefile::new(vec, 3).unwrap().target_name, "foobar".to_string());
+    assert_eq!(
+        Codefile::new(&vec, 3).unwrap().target_name,
+        "foobar".to_string()
+    );
 }
 
 #[test]
 #[should_panic]
 fn panics_with_bad_flag() {
-    let file = Codefile::new(vec![
-        "run".to_string(),
-        "-o".to_string(),
-        "main.c".to_string(),
-    ], 3);
+    let vec = vec!["run".to_string(), "-o".to_string(), "main.c".to_string()];
+    let file = Codefile::new(&vec, 3);
     file.unwrap();
-} 
+}
 
 #[test]
 #[should_panic]
 fn panics_with_bad_args() {
-    let _file = Codefile::new(Vec::new(), 0).unwrap();
+    let _file = Codefile::new(&Vec::new(), 0).unwrap();
 }
 
+#[test]
+#[should_panic]
+fn panics_with_bad_filetype() {
+    let vec: Vec<String> = vec!["run", "my_file.txt"].iter().map(|s|s.to_string()).collect();
+    let _ = Codefile::new(&vec, get_filename_index(&vec));
+}
