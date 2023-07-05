b6c86161ca97b542001a9a07b529206030e74f17
oritzau <oritzau2021@gmail.com>
2023-07-04 23:42:39 -0400

    Updated structure to give more helpful error messages

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
-use run::code_file::{get_filename_index, Codefile};
+use run::codefile::{Codefile, get_filename_index};
 use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    assert_ne!(args.len(), 1);
     // Crate should not be run without args, "cargo run" has no meaning
     
     let index = get_filename_index(&args); // Index of filename inside vector of args
 
-    // If creation fails, Codefile::new() returns none and the program ends with no side effects
+    // If creation fails, Codefile::new() returns Option::None and the program ends with no side effects
     let file: Option<Codefile> = Codefile::new(&args, index);
     if let Some(f) = file {
-        f.execute()
+        if f.execute().is_err() {
+            println!("Your codefile was successfully created, but ran into an issue executing");
+        }
     } else {
         // Todo: give more helpful errors
-        println!("Something went wrong");
+        println!("Your codefile was not successfully created");
     }
 }
